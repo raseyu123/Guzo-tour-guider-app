9bc934df1656f7ecaa7083a3f5b18805bdc9c0b1
Eyoel <eyoelsitotaw6@gmail.com>
2019-07-01 23:04:11 +0300

    guzo

--- app/src/main/java/com/example/guzo/Museums/MuseumLists.java
+++ app/src/main/java/com/example/guzo/Museums/MuseumLists.java
@@ -30,10 +30,14 @@ import com.squareup.picasso.Picasso;
  * A simple {@link Fragment} subclass.
  */
 public class MuseumLists extends Fragment {
-
+    private FirebaseRecyclerAdapter<Museums, MuseumListsViewHolder> adapter ;
+    private RecyclerView recycler_museum;
+    RecyclerView.LayoutManager layoutManager;
     Context context;
-
-       static MuseumLists instance;
+    private FirebaseDatabase database;
+    private DatabaseReference museumLists;
+    String cityId="";
+    static MuseumLists instance;
 
 
 
@@ -55,23 +59,54 @@ public class MuseumLists extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v= inflater.inflate(R.layout.fragment_museum_lists, container, false);
-
+        cityId=this.getArguments().getString("Id");
+        Toast.makeText(getContext(), cityId, Toast.LENGTH_SHORT).show();
 
         return v;
 
     }
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        recycler_museum=view.findViewById (R.id.recycler_museum_lists);
+        recycler_museum.setLayoutManager(new GridLayoutManager(getContext(),2));
+//         LayoutAnimationController controller= AnimationUtils.loadLayoutAnimation(recycler_museum.getContext()
+//         ,R.anim.layout_fall_down);
+//         recycler_museum.setLayoutAnimation(controller);
+
+        database = FirebaseDatabase.getInstance();
+        museumLists = database.getReference("Museums");
+        loadMuseum(cityId);
+    }
+    private void loadMuseum(String cityId ) {
+
+        adapter= new FirebaseRecyclerAdapter<Museums, MuseumListsViewHolder>(Museums.class,
+                R.layout.activity_museums_item,
+                MuseumListsViewHolder.class,
+
+                museumLists.orderByChild("CityId").equalTo(cityId)) {
 
 
 
+            protected void populateViewHolder(MuseumListsViewHolder museumListsViewHolder, Museums model , int position) {
 
+                museumListsViewHolder.museumName.setText(model.getName());
+                Picasso.get().load(model.getImage()).into(museumListsViewHolder.museumPic);
+                museumListsViewHolder.setItemClickListener(new ItemClickListener() {
+                    @Override
+                    public void onClick(View view, int position, boolean isLongClick) {
+                        Intent museumDetail = new Intent(getContext(), CitySelected.class);
+                        museumDetail.putExtra("CityId",adapter.getRef(position).getKey());
+                        startActivity(museumDetail);
+                        // Toast.makeText(MainActivity.this, ""+clickItem.getName(), Toast.LENGTH_SHORT).show();
+                    }
+                });}
+        };
 
+        recycler_museum.setAdapter(adapter);
 
-
-
-
-
-
-
-
+        //animation
+        recycler_museum.getAdapter().notifyDataSetChanged();
+        recycler_museum.scheduleLayoutAnimation();
+    }
 
 }
